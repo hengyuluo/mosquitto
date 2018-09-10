e373228424e646d1fdc918427242f45a86e92400
罗珩育 <408399281@qq.com>
2018-09-10 11:40:07 +0800

    7

--- src/loop.c
+++ src/loop.c
@@ -726,29 +726,25 @@ void do_disconnect(struct mosquitto_db *db, struct mosquitto *context)
 			}else{
 				log__printf(NULL, MOSQ_LOG_NOTICE, "Client %s disconnected.", id);
 			}
-		
-			printf("6.1\n");
+		}
 #ifdef WITH_EPOLL
 		if (context->sock != INVALID_SOCKET && epoll_ctl(db->epollfd, EPOLL_CTL_DEL, context->sock, &ev) == -1) {
 			log__printf(NULL, MOSQ_LOG_DEBUG, "Error in epoll disconnecting: %s", strerror(errno));
 		}
 #endif
 		context__disconnect(db, context);
-		printf("6.2\n");
 #ifdef WITH_BRIDGE
 		if(context->clean_session && !context->bridge){
 #else
 		if(context->clean_session){
 #endif
 			context__add_to_disused(db, context);
-			printf("6.3\n");
 			if(context->id){
 				HASH_DELETE(hh_id, db->contexts_by_id, context);
 				mosquitto__free(context->id);
 				context->id = NULL;
 			}
 		}
-		printf("6.4\n");
 		context->state = mosq_cs_disconnected;
 	}
 	printf("7\n");
@@ -760,6 +756,7 @@ static void loop_handle_reads_writes(struct mosquitto_db *db, mosq_sock_t sock, 
 #else
 static void loop_handle_reads_writes(struct mosquitto_db *db, struct pollfd *pollfds)
 #endif
+{
 	struct mosquitto *context;
 #ifndef WITH_EPOLL
 	struct mosquitto *ctxt_tmp;
